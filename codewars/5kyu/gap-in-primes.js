55a13cddc9df620ca2e5f9ecb7ebf91fb6113e42
Ali Bayramli <alibayramli97@gmail.com>
2020-05-06 15:51:01 +0200

    Create gap-in-primes.js

---
+++ codewars/5kyu/gap-in-primes.js
@@ -0,0 +1,15 @@
+
+function gap(g, m, n) {
+    let lastPrime = 0;
+    let isPrime = function (x) {
+        for (let i = 2; i * i <= x; i++) { if (x % i == 0) return false; } return true;
+    }
+
+    for (let i = m; i <= n; i++)
+        if (isPrime(i)) {
+            if (i - lastPrime == g) return [lastPrime, i];
+            else lastPrime = i;
+        }
+
+    return null;
+}
